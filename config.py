e82848ef129ffe73f4d709195fa35ed399221768
ayush4661 <ayush4661@gmail.com>
2023-12-03 23:43:45 +0530

    Creating FastAPI app and adding db connection settings

---
+++ config.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from db import SessionLocal, engine
+import models
+
+app = FastAPI()
+
+models.Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
